6ee52786106f70e4e19e8fe4e242d6f1c9a150fd
Sigmund Hansen <sigmund.hansen@uptimeconsulting.no>
2023-04-04 12:46:57 +0200

    Update dependencies.

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
 ;; src/drift/drift_version.clj! 
 (defproject drift "1.5.4-SNAPSHOT"
   :description "Drift is a rails like migration framework for Clojure."
-  :dependencies [[clojure-tools "1.1.2"]
-                 [org.clojure/tools.logging "0.2.3"]]
-  :profiles {:dev {:dependencies [[log4j/log4j "1.2.16"]]}}
+  :dependencies [[clojure-tools "1.1.3"]
+                 [org.clojure/tools.logging "1.2.4"]]
+  :profiles {:dev {:dependencies [[log4j/log4j "1.2.17"]]}}
 
   :aot [drift.listener-protocol drift.Drift])
